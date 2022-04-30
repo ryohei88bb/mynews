e7cf327d7c7c2fcd4ca6efc2171ad3fb8d46f909
ryohei88bb <ryohei88bb@gmail.com>
2022-04-30 01:43:00 +0000

    la16.n

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -31,11 +31,26 @@ class ProfileController extends Controller
 
     public function edit()
     {
+        $profile = Profile::find($request->id);
+      if (empty($profile)) {
+        abort(404);    
+      }
         return view('admin.profile.edit');
     }
 
     public function update()
     {
+      $this->validate($request, Profile::$rules);
+      $profile = Profile::find($request->id);
+      
+      $profile_form = $request->all();
+
+      unset($profile_form['remove']);
+      unset($profile_form['_token']);
+
+      // 該当するデータを上書きして保存する
+      $news->fill($news_form)->save();
+      
         return redirect('admin/profile/edit');
         
     }
