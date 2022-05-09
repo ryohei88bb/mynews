bd05564c31d85876cf1ac4a0e09cbc48c0a1c61a
ryohei88bb <ryohei88bb@gmail.com>
2022-05-09 01:44:55 +0000

    heroku2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,9 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        if (\App::environment('production')) {
+        \URL::forceScheme('https');
+    }
     }
 
     /**
